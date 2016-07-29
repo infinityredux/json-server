d09cb49b3df71d442fb0e373290f4215b74d5fcd
Nick <kedomir@gmail.com>
2016-07-29 01:19:09 +0100

    Using the old wip branch to create config file loading.

--- start.js
+++ start.js
@@ -2,18 +2,32 @@
  * Created by Nick on 05/01/2016.
  */
 
+var fs = require('fs');
+
 var json = require('./jsonServer.js');
 
 var config = {
-    name: 'Infinity',
+    name: 'Local',
     port: 8000,
-    host: 'projects.infinityredux.net',
-    shutdownPassword: 'password',
+    host: 'localhost',
+    shutdownPassword: 'randomPassword',
     fileLoad: [
         './routes/showRequest.js',
         './routes/eve.js'
     ]
 };
+
+if (process.argv.length > 2) {
+    console.log('Loading config file');
+    try {
+        var contents = fs.readFileSync(process.argv[2]);
+        var results = JSON.parse(contents);
+        if (results) {config = results;}
+    }
+    catch (err) {
+        console.log('Error loading config file, reverting to defaults.');
+    }
+}
 
 json.init(config);
 json.listen();
